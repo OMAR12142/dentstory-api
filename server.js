c5b2f3c59e677538861062bebbdaaa8187725f0f
Omar selema <omarselema52@gmail.com>
2026-06-20 01:41:31 +0300

    Update server.js

--- server.js
+++ server.js
@@ -31,6 +31,7 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:3000',
   'https://dentstory.vercel.app',
+  'https://dentstory.site',
   ...(process.env.CLIENT_URL ? process.env.CLIENT_URL.split(',').map(u => u.trim()) : []),
 ];
 
